0804a1267c93d7963b7b192315b46fa7a892aace
Deeptesh chaudhari <diptesh151097@gmail.com>
2023-10-27 22:15:52 +0530

    .

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ import express from 'express';
 import path from 'path';
 import routes from './routes/index.js'
 import mongoose from "mongoose";
-const PORT = process.env.PORT || 3001
 const mongoDBURL = process.env.mongoDBURL || 'mongodb://localhost:27017'
 const app = express();
 
