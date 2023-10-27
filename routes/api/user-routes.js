d67246ed5343f2cf6b1647c55fb285e489cf8299
Deeptesh chaudhari <diptesh151097@gmail.com>
2023-10-27 19:11:05 +0530

    .

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,18 +1,18 @@
 const { createUser, login, getSingleUser } = require("../../controllers/user-controller.js");
 import express from "express";
-const router = express.Router()
+const userRouter = express.Router()
 
 // import middleware
 import authMiddleware from "../../utils/auth.js";
 
 // put authMiddleware anywhere we need to send a token for verification of user
 // /api/user for user signup
-router.route("/").post(createUser)
+userRouter.route("/").post(createUser)
 
 // /api/user/login for user login
-router.route("/login").post(login);
+userRouter.route("/login").post(login);
 
 // /api/user/me to get single user data
-router.route('/me').get(authMiddleware, getSingleUser);
+userRouter.route('/me').get(authMiddleware, getSingleUser);
 
-module.exports = router;
+module.exports = userRouter;
