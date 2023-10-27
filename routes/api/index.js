8e15fb0830c30f17a8b3be032b3da37e36a4f88f
Deeptesh chaudhari <diptesh151097@gmail.com>
2023-10-27 16:55:49 +0530

    .

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,8 +2,8 @@ import express from "express";
 
 const apiRoutes = express.Router()
 
-import exerciseRoutes from "./exercise-routes";
-import userRoutes from "./user-routes";
+import exerciseRoutes from "../api/exercise-routes.js";
+import userRoutes from "../api/user-routes.js";
 
 router.use("/user", userRoutes);
 router.use("/exercise", exerciseRoutes);
