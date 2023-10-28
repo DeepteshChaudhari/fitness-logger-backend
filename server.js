0a06a27816d44f867e46b1dd05fd6e99a945cdda
Deeptesh chaudhari <diptesh151097@gmail.com>
2023-10-28 16:14:53 +0530

    .

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-import express from 'express'
+import express from 'express';
 import path from 'path';
-import apiRoutes from './routes/api/index.js';
-import db from './config/connection.js'
-
-const PORT = process.env.PORT || 3001;
+import routes from './routes/index.js'
+import mongoose from "mongoose";
+const mongoDBURL = process.env.mongoDBURL || 'mongodb://localhost:27017'
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
@@ -11,17 +10,18 @@ app.use(express.json());
 
 // Serve up static assets
 //if (process.env.NODE_ENV === "production") {
-//  app.use(express.static(path.join(__dirname, "../client/build")));
+// app.use(express.static(path.join(__dirname  , "../client/build")));
 //}
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-app.use(apiRoutes);
-
-db.once("open", () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-  });
-});
+app.use(routes);
+mongoose.set('strictQuery', true);
+mongoose.connect(mongoDBURL, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+}).then(() => console.log("DataBase Connected")).catch((err) => {
+  console.log(err);
+})
