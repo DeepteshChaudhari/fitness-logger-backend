b304265e0568e5c05a7cbac3f5f30a8b49e0cfcf
Deeptesh chaudhari <diptesh151097@gmail.com>
2023-10-28 16:23:59 +0530

    .

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,16 @@
 import mongoose from "mongoose";
 
-mongoose.set("strictQuery", false);
+// Remove the line below, it's not necessary
+// mongoose.set("strictQuery", false);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-}, err => {
-  if (err) throw err;
-  console.log('Connected to MongoDB!')
-}
-
-);
+}).then(() => {
+  console.log('Connected to MongoDB!');
+}).catch((err) => {
+  console.error('Error connecting to MongoDB:', err);
+});
 
 const db = mongoose.connection;
-export default db
+export default db;
