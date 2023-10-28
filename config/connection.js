e3051526e32f2e7440689371483a3155d16e875d
Deeptesh chaudhari <diptesh151097@gmail.com>
2023-10-28 15:58:54 +0530

    .

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 mongoose.set("strictQuery", false);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }, err => {
