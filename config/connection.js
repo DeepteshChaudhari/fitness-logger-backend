a579860dcf49a39677d6b5f1c4357fa1284e3c51
Deeptesh chaudhari <diptesh151097@gmail.com>
2023-10-28 15:39:52 +0530

    .

--- config/connection.js
+++ config/connection.js
@@ -1,2 +1,16 @@
-export const mongoDBURL = process.env.mongoDBURL
-export const PORT = process.env.PORT
+import mongoose from "mongoose";
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}, err => {
+  if (err) throw err;
+  console.log('Connected to MongoDB!')
+}
+
+);
+
+const db = mongoose.connection;
+export default db
--- server.js
+++ server.js
@@ -1,27 +1,28 @@
-import express from 'express';
+import express from 'express'
 import path from 'path';
-import routes from './routes/index.js'
-import mongoose from "mongoose";
-const mongoDBURL = process.env.mongoDBURL || 'mongodb://localhost:27017'
+import apiRoutes from './routes/api/index.js';
+const db = require("./config/connection");
+import db from './config/connection.js'
+
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Serve up static assets
-//if (process.env.NODE_ENV === "production") {
-// app.use(express.static(path.join(__dirname  , "../client/build")));
-//}
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 app.use(routes);
-mongoose.set('strictQuery', true);
-mongoose.connect(mongoDBURL, {
-  useUnifiedTopology: true,
-  useNewUrlParser: true
-}).then(() => console.log("DataBase Connected")).catch((err) => {
-  console.log(err);
-})
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
