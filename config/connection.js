69082f975d09b21acf4c43f15748e598c2fba290
Deeptesh chaudhari <diptesh151097@gmail.com>
2023-10-29 20:16:23 +0530

    .

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,11 @@ const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", false);
 
-mongoose.connect("mongodb://localhost:27017", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}, err => {
-  if (err) throw err;
-  console.log('Connected to MongoDB!')
-}
+mongoose.connect("mongodb://localhost:27017",
+  err => {
+    if (err) throw err;
+    console.log('Connected to MongoDB!')
+  }
 
 );
 
