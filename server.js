3ff6b9e85611f91b85e53753d4f97d3bf9c1b252
Deeptesh chaudhari <diptesh151097@gmail.com>
2023-10-30 02:28:10 +0530

    .

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.js'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 app.use(routes);
