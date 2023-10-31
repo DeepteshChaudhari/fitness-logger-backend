d5b91ec1ea1d9d77e09725dd4dd72ce49a129625
Deeptesh chaudhari <diptesh151097@gmail.com>
2023-10-31 17:27:46 +0530

    .

--- server.js
+++ server.js
@@ -5,27 +5,19 @@ const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-//const corsOptions = {
-//origin: [
-//"http://localhost:3000",
-//"https://fitnesslogger.netlify.app",
-//],
-//credentials: true,
-//methods: ["GET", "POST", "DELETE", "PUT"],
-//};
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-//app.use(cors(corsOptions));
 
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../fitness-logger-frontend/build/")));
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'https://fitnesslogger.netlify.app'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
+
 app.use(routes);
 
 db.once("open", () => {
