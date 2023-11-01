e70c81b7c45a5cc9d49366adfee96297bab542a5
Deeptesh chaudhari <diptesh151097@gmail.com>
2023-11-01 14:35:01 +0530

    .

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,19 @@
+
 const router = require("express").Router();
 const path = require("path");
 const apiRoutes = require("./api");
 
 router.use("/api", apiRoutes);
 
-// connect with react router
-// serve up react front-end in production
+// Serve up the React front-end in production
 router.use((req, res) => {
-  res.sendFile(path.join(__dirname, "../../build/index.html"));
+  res.sendFile(path.join(__dirname, "../../build/index.html"), (err) => {
+    if (err) {
+      // Handle the error here, for example, send a 500 Internal Server Error response.
+      res.status(500).send("Internal Server Error");
+    }
+  });
 });
 
 module.exports = router;
+
