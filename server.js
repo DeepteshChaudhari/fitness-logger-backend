ec08e43416ed28fd29cdaf79129b2a3b332be99d
Deeptesh chaudhari <diptesh151097@gmail.com>
2023-11-07 20:21:19 +0530

    .

--- server.js
+++ server.js
@@ -18,12 +18,12 @@ app.use(express.json());
 
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static("/client/build"));
 }
 
-//app.get('*', (req, res) => {
-//  res.sendFile(path.join(__dirname, '../public/index.html'));
-//});
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 app.use(routes);
 app.use(cors(corsOpts));
