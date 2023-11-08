2ea9f52d77db359262f2c38e5e0ab047b4ba5fea
Deeptesh chaudhari <diptesh151097@gmail.com>
2023-11-08 17:07:04 +0530

    .

--- server.js
+++ server.js
@@ -9,14 +9,21 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(routes);
-//* Serve static assets in production, must be at this location of this file
-if (process.env.NODE_ENV === 'production') {
-  //*Set static folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
-app.listen(PORT, () => console.log(`Server started port ${PORT}`));
+app.use(routes);
+
+app.get("/", (req, res) => {
+  res.status(200).send("API is running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
