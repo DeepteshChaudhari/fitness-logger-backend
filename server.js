39bc2355d86c78acf599bc4e9b885a3a9fb42ff2
Deeptesh chaudhari <diptesh151097@gmail.com>
2023-11-08 14:47:13 +0530

    .

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const path = require("path");
 const routes = require("./routes");
 const db = require("./config/connection");
-const PORT = process.env.PORT || 3000;
+
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
@@ -10,12 +11,12 @@ app.use(express.json());
 
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("/client/build"));
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.get('*', (req, res) => {
-  res.sendFile('../client/build/index.html');
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
 app.use(routes);
 
