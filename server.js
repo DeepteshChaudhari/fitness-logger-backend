d59233e6d02c4816de39840f81950e5b07b1a451
Deeptesh chaudhari <diptesh151097@gmail.com>
2023-11-08 17:20:09 +0530

    .

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "https://fitnesslogger.netlify.app/")));
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
 // app.get('*', (req, res) => {
