6de2233c8762b5ecd357e4e46744b1f340df3003
Deeptesh chaudhari <diptesh151097@gmail.com>
2023-11-08 15:34:57 +0530

    .

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,8 @@ router.use("/api", apiRoutes);
 
 // connect with react router
 // serve up react front-end in production
-router.use((req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
+//router.use((req, res) => {
+//res.sendFile(path.join(__dirname, "index.html"));
+//});
 
 module.exports = router;
