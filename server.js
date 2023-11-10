cbf13ca7195e3724d5044e1631bb01957e204d53
Deeptesh chaudhari <diptesh151097@gmail.com>
2023-11-10 15:06:50 +0530

    .

--- server.js
+++ server.js
@@ -10,16 +10,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Serve up static assets
-if (process.env.MODE === "production") {
-  app.use(express.static(path.join(__dirname, "build")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "..", "client", "build")));
+}
 
-  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
-} else {
-  app.get("/", (req, res) => {
-    res.send("Welcome to the home page");
-  });
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
-} app.use(routes);
+app.use(routes);
 
 db.once("open", () => {
   app.listen(PORT, () => {
